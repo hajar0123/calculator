2075e1e1b7e99e07f90c57824d06cd9af6a7707b
hajar0123 <hajarsniba2001@gmail.com>
2023-12-21 10:18:09 -0500

    test

--- src/main/java/com/hajar/calculator/Calculator.java
+++ src/main/java/com/hajar/calculator/Calculator.java
@@ -1,8 +1,9 @@
 package com.hajar.calculator;
 import org.springframework.stereotype.Service;
+
 @Service
 public class Calculator {
-public int sum(int a, int b) {
-return a + b;
-}
+	public int sum(int a, int b) {
+		return a + b;
+	}
 }
--- src/test/java/com/hajar/calculator/CalculatorTest.java
+++ src/test/java/com/hajar/calculator/CalculatorTest.java
@@ -1,8 +1,10 @@
 package com.hajar.calculator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 private Calculator calculator = new Calculator();
+
 @Test
 public void testSum() {
 assertEquals(8, calculator.sum(3, 5));
